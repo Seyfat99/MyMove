0a849e2a5f27c24d48f87961d9a8f0b21feb93c3
Seyfat Khamidov <skhamidov@rv-clt-skhamidov.redventures.net>
2018-10-25 09:16:54 -0400

    added some styles to rows

--- scripts.js
+++ scripts.js
@@ -5,12 +5,12 @@ window.onscroll = function () {
 
 var nav = document.querySelector('.top-nav');
 
-var sticky = nav.offsetTop;
+var sticky = nav.offsetTop;   // number of pixels from the closest parent element
 function myFunction() {
-    if (window.pageYOffset > sticky) {
-        nav.classList.add("stickyNav");
+    if (window.pageYOffset > sticky) {      // when page scrolls, the page offset becomes greater than 0 
+        nav.classList.add("nav-background");     // and adds background color
     } else {
-        nav.classList.remove("stickyNav");
+        nav.classList.remove("nav-background");
     }
 }
 
